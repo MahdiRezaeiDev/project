fb2af0450476ac1d9839029fc80b2923ad6aa221
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-01-09 10:15:04 +0430

    Update time formatting in partnerFactor.php and yadakFactor.php to use 12-hour format

--- views/factor/partnerFactor.php
+++ views/factor/partnerFactor.php
@@ -67,7 +67,7 @@ require_once '../../layouts/callcenter/sidebar.php';
 <script src="./assets/js/displayFactor/factor.js"></script>
 <script>
     const time = '<?= $BillInfo['created_at']; ?>';
-    const now = moment(time).locale('fa').format('H:M');
+    const now = moment(time).locale('fa').format('h:Ma');
     displayFinanceBill();
     displayFinanceCustomer();
     displayFinanceBillDetails();
