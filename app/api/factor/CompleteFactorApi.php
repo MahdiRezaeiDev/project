432b1cfeb3c65fc9d718c4e09f7da9cf255a46e0
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-02-13 11:56:14 +0430

    Add SMS sending after completing factor update in CompleteFactorApi

--- app/api/factor/CompleteFactorApi.php
+++ app/api/factor/CompleteFactorApi.php
@@ -92,7 +92,7 @@ if (isset($_POST['updateCompleteFactor'])) {
         UpdateCompletedBill($factorInfo, $customer_id);
         CreateBillItems($factorInfo, $factorItems);
         getSimilarGoods($factorItems, $factorInfo->id, $customerInfo, $factorInfo->billNO, $factorInfo->partner, $factorInfo->totalPrice, $factorInfo->date, true);
-        sendSMS($customerInfo, $factorInfo, $factorItems, $factorNumber);
+        // sendSMS($customerInfo, $factorInfo, $factorItems, $factorNumber);
     } catch (Exception $e) {
         // An error occurred, rollback the transaction
         PDO_CONNECTION->rollback();
