627e18a03078d333ed99f946209a7b5d4c3f5934
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-06-03 10:42:43 +0430

    Update username validation in setup_loading function to include additional users

--- app/controller/callcenter/OrderedPriceController.php
+++ app/controller/callcenter/OrderedPriceController.php
@@ -62,7 +62,7 @@ function setup_loading($customer, $completeCode, $notification = null)
 
     $sql = "INSERT INTO shop.searches (partNumber, user_id) VALUES (:request, :user_id)";
 
-    if (!in_array($_SESSION['username'], ['mahdi', 'niyayesh'])) :
+    if (!in_array($_SESSION['username'], ['mahdi', 'niyayesh', 'aliakbar', 'amirmohammad', 'amani'])) :
         foreach ($explodedCodes as $code) {
             $stmt = PDO_CONNECTION->prepare($sql);
             $stmt->bindParam(':request', $code, PDO::PARAM_STR);
